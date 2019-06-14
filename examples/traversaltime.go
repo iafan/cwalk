679494e6889bb113757f30ad0cb74150fc464de2
Seth <18116602+sethkor@users.noreply.github.com>
2019-06-14 20:00:19 +1000

    Tidy of exmaples and tests

--- examples/traversaltime.go
+++ examples/traversaltime.go
@@ -13,8 +13,6 @@ import (
 var fileCount int32
 var folderCount int32
 var errorCount int32
-var openFiles = make(chan string)
-var fileSlice []*os.File
 
 // This callback simply counts files and folders.
 //
@@ -23,31 +21,10 @@ var fileSlice []*os.File
 func callback(path string, info os.FileInfo, err error) error {
 	if err != nil {
 		atomic.AddInt32(&errorCount, 1)
-		return err
 	} else {
 		if info.IsDir() {
 			atomic.AddInt32(&folderCount, 1)
 		} else {
-			atomic.AddInt32(&fileCount, 1)
-		}
-	}
-	return nil
-}
-
-// This callback simply counts files and folders and also opens the file.
-//
-// Note that the callback function should be thread-safe
-// (this is why we use "atomic.AddInt32()" function to increment counters).
-func errorCallback(path string, info os.FileInfo, err error) error {
-	if err != nil {
-		atomic.AddInt32(&errorCount, 1)
-		return err
-	} else {
-		if info.IsDir() {
-			atomic.AddInt32(&folderCount, 1)
-		} else {
-			f, _ := os.Open(path)
-			fileSlice = append(fileSlice, f)
 			atomic.AddInt32(&fileCount, 1)
 		}
 	}
@@ -70,17 +47,12 @@ func main() {
 	fmt.Print("Running concurrent version... ")
 	start := time.Now()
 
-	err := cwalk.Walk(dir, callback)
+	cwalk.Walk(dir, callback)
 
 	fmt.Printf("done in %s\n", time.Since(start))
 	fmt.Printf("\t%d directories found\n", folderCount)
 	fmt.Printf("\t%d files found\n", fileCount)
-	fmt.Printf("\t%d errors detected by the callback\n", errorCount)
-
-	if err != nil {
-		fmt.Printf("\t%d errors returned by cwalk\n", len(err.(cwalk.WalkerErrorList).ErrorList))
-		fmt.Printf("Error :\n%s\n", err.Error())
-	}
+	fmt.Printf("\t%d errors found\n", errorCount)
 
 	// run the standard (single-threaded) version
 
@@ -91,42 +63,10 @@ func main() {
 	fmt.Print("Running standard version... ")
 	start = time.Now()
 
-	err = filepath.Walk(dir, callback)
+	filepath.Walk(dir, callback)
 
 	fmt.Printf("done in %s\n", time.Since(start))
 	fmt.Printf("\t%d directories found\n", folderCount)
 	fmt.Printf("\t%d files found\n", fileCount)
-	fmt.Printf("\t%d errors detected by the callback\n", errorCount)
-
-	if err != nil {
-		fmt.Printf("Error : %s\n", err.Error())
-	}
-
-	// run the concurrent triggering errors version
-
-	folderCount = 0
-	fileCount = 0
-	errorCount = 0
-
-	fmt.Print("Running concurrent version to trigger errors...\n")
-	fmt.Print("Errors are triggered by exceeding file limits by opening every file... ")
-
-	start = time.Now()
-
-	//start the open files func
-	time.Sleep(1000)
-
-	err = cwalk.Walk(dir, errorCallback)
-
-	close(openFiles)
-
-	fmt.Printf("done in %s\n", time.Since(start))
-	fmt.Printf("\t%d directories found\n", folderCount)
-	fmt.Printf("\t%d files found\n", fileCount)
-	fmt.Printf("\t%d errors detected by the callback\n", errorCount)
-
-	if err != nil {
-		fmt.Printf("\t%d errors returned by cwalk\n", len(err.(cwalk.WalkerErrorList).ErrorList))
-		fmt.Printf("Error :\n%s\n", err.Error())
-	}
+	fmt.Printf("\t%d errors found\n", errorCount)
 }
