2e77bba6671eda29bc3fdc990dcd685614d05a36
Igor Afanasyev <afan@evernote.com>
2019-06-14 13:18:13 -0700

    Improve traversaltime by allowing to follow symlinks and add delay

--- bin/traversaltime.go
+++ bin/traversaltime.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -13,6 +14,9 @@ import (
 var fileCount int32
 var folderCount int32
 var errorCount int32
+
+var followSymlinks bool
+var processingTime time.Duration
 
 // This callback simply counts files and folders.
 //
@@ -28,15 +32,32 @@ func callback(path string, info os.FileInfo, err error) error {
 			atomic.AddInt32(&fileCount, 1)
 		}
 	}
+	if processingTime > 0 {
+		time.Sleep(processingTime)
+	}
 	return nil
 }
 
+func init() {
+	flag.BoolVar(&followSymlinks, "follow-symlinks", false, "When specified, directory symlinks will be processed and followed")
+	flag.BoolVar(&followSymlinks, "f", false, "Shorthand for -follow-symlinks")
+
+	flag.DurationVar(&processingTime, "file-processing-time", 0, "An artificial delay, for each file processed, to imitate actual work. Omitting this parameter means no delay. Example: 50ms")
+	flag.DurationVar(&processingTime, "t", 0, "Shorthand for -file-processing-time")
+}
+
 func main() {
-	if len(os.Args) < 2 || os.Args[1] == "" {
-		fmt.Println("Usage: traversaltime <directory-to-scan>")
+	flag.Parse()
+
+	if len(flag.Args()) < 1 || flag.Args()[0] == "" {
+		fmt.Println("Usage:")
+		fmt.Println("  traversaltime [-f] [-t N] <directory-to-scan>")
+		fmt.Println("Options:")
+		flag.PrintDefaults()
 		os.Exit(0)
 	}
-	dir := os.Args[1]
+	dir := flag.Args()[0]
+	fmt.Println("Directory:", dir)
 
 	// run the concurrent version
 
@@ -44,15 +65,24 @@ func main() {
 	fileCount = 0
 	errorCount = 0
 
-	fmt.Print("Running concurrent version... ")
 	start := time.Now()
+	var err error
 
-	cwalk.Walk(dir, callback)
+	if followSymlinks {
+		fmt.Printf("Running a concurrent version that follows symlinks with %d workers and %s file processing time... ", cwalk.NumWorkers, processingTime)
+		err = cwalk.WalkWithSymlinks(dir, callback)
+	} else {
+		fmt.Printf("Running a concurrent version that doesn't follow symlinks with %d workers and %s file processing time... ", cwalk.NumWorkers, processingTime)
+		err = cwalk.Walk(dir, callback)
+	}
 
 	fmt.Printf("done in %s\n", time.Since(start))
 	fmt.Printf("\t%d directories found\n", folderCount)
 	fmt.Printf("\t%d files found\n", fileCount)
 	fmt.Printf("\t%d errors found\n", errorCount)
+	if err != nil {
+		fmt.Printf("\nErrors: %s\n\n", err)
+	}
 
 	// run the standard (single-threaded) version
 
@@ -60,13 +90,16 @@ func main() {
 	fileCount = 0
 	errorCount = 0
 
-	fmt.Print("Running standard version... ")
+	fmt.Printf("Running a standard version (single-threaded, doesn't follow symlinks) with %s file processing time... ", processingTime)
 	start = time.Now()
 
-	filepath.Walk(dir, callback)
+	err = filepath.Walk(dir, callback)
 
 	fmt.Printf("done in %s\n", time.Since(start))
 	fmt.Printf("\t%d directories found\n", folderCount)
 	fmt.Printf("\t%d files found\n", fileCount)
 	fmt.Printf("\t%d errors found\n", errorCount)
+	if err != nil {
+		fmt.Printf("\nError: %s\n\n", err)
+	}
 }
