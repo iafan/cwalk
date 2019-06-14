b4f72f5e5b7980b24c5f75a7ee84050e122f6fd5
Seth <18116602+sethkor@users.noreply.github.com>
2019-06-15 04:12:15 +1000

    Gather and report errors when walking files (fix #1)
    This is a squash merge of PR #2 by @sethkor

--- cwalk.go
+++ cwalk.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 	"sync"
 )
 
@@ -19,11 +20,42 @@ var BufferSize = NumWorkers
 // to a walker function, does not point to a directory
 var ErrNotDir = errors.New("Not a directory")
 
+// A struct to store individual errors reported from each worker routine
+type WalkerError struct {
+	error error
+	path  string
+}
+
+// A struct to store a list of errors reported from all worker routine
+type WalkerErrorList struct {
+	ErrorList []WalkerError
+}
+
+// Implement the error interface for WalkerError
+func (we WalkerError) Error() string {
+	return we.error.Error()
+}
+
+// Implement the error interface fo WalkerErrorList
+func (wel WalkerErrorList) Error() string {
+	if len(wel.ErrorList) > 0 {
+		out := make([]string, len(wel.ErrorList))
+		for i, err := range wel.ErrorList {
+			out[i] = err.Error()
+		}
+		return strings.Join(out, "\n")
+	}
+	return ""
+}
+
 // Walker is constructed for each Walk() function invocation
 type Walker struct {
-	wg       sync.WaitGroup
-	jobs     chan string
-	walkFunc filepath.WalkFunc
+	wg        sync.WaitGroup
+	ewg       sync.WaitGroup // a separate wg for error collection
+	jobs      chan string
+	walkFunc  filepath.WalkFunc
+	errors    chan WalkerError
+	errorList WalkerErrorList // this is where we store the errors as we go
 }
 
 // the readDirNames function below was taken from the original
@@ -44,6 +76,15 @@ func readDirNames(dirname string) ([]string, error) {
 		return nil, err
 	}
 	return names, nil
+}
+
+// collectErrors processes any any errors passed via the error channel
+// and stores them in the errorList
+func (w *Walker) collectErrors() {
+	defer w.ewg.Done()
+	for err := range w.errors {
+		w.errorList.ErrorList = append(w.errorList.ErrorList, err)
+	}
 }
 
 // processPath processes one directory and adds
@@ -81,7 +122,13 @@ func (w *Walker) addJob(path string) {
 	case w.jobs <- path: // ok
 	default: // buffer overflow
 		// process job synchronously
-		w.processPath(path)
+		err := w.processPath(path)
+		if err != nil {
+			w.errors <- WalkerError{
+				error: err,
+				path:  path,
+			}
+		}
 	}
 }
 
@@ -89,8 +136,15 @@ func (w *Walker) addJob(path string) {
 // until the jobs channel is explicitly closed
 func (w *Walker) worker() {
 	for path := range w.jobs {
-		w.processPath(path)
+		err := w.processPath(path)
+		if err != nil {
+			w.errors <- WalkerError{
+				error: err,
+				path:  path,
+			}
+		}
 	}
+
 }
 
 // Walk recursively descends into subdirectories,
@@ -98,11 +152,17 @@ func (w *Walker) worker() {
 // in the tree, including root directory.
 // Walk does not follow symbolic links.
 func (w *Walker) Walk(root string, walkFn filepath.WalkFunc) error {
+	w.errors = make(chan WalkerError, BufferSize)
 	w.jobs = make(chan string, BufferSize)
 	w.walkFunc = walkFn
 
+	w.ewg.Add(1) // a separate error waitgroup so we wait until all errors are reported before exiting
+	go w.collectErrors()
+
 	info, err := os.Lstat(root)
-	err = w.walkFunc(root, info, err)
+	if err == nil {
+		err = w.walkFunc(root, info, err)
+	}
 	if err != nil {
 		return err
 	}
@@ -114,10 +174,15 @@ func (w *Walker) Walk(root string, walkFn filepath.WalkFunc) error {
 	for n := 1; n <= NumWorkers; n++ {
 		go w.worker()
 	}
-	w.addJob(root) // add root path as a first job
-	w.wg.Wait()    // wait till all paths are processed
-	close(w.jobs)  // signal workers to close
+	w.addJob(root)  // add root path as a first job
+	w.wg.Wait()     // wait till all paths are processed
+	close(w.jobs)   // signal workers to close
+	close(w.errors) // signal errors to close
+	w.ewg.Wait()    // wait for all errors to be collected
 
+	if len(w.errorList.ErrorList) > 0 {
+		return w.errorList
+	}
 	return nil
 }
 
---
+++ tests/error-handling.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync/atomic"
+	"time"
+
+	"github.com/iafan/cwalk"
+)
+
+// This example takes bin/traversaltime.go program as a basis, but, for the sake of ean xample, starts to accumulate
+// open handles processing them slowly to artificially cause errors related to exceeding the number of open file handles
+
+var fileCount int32
+var folderCount int32
+var errorCount int32
+var openFiles = make(chan string)
+var fileSlice []*os.File
+
+// This callback simply counts files and folders.
+//
+// Note that the callback function should be thread-safe
+// (this is why we use "atomic.AddInt32()" function to increment counters).
+func callback(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		atomic.AddInt32(&errorCount, 1)
+		return err
+	} else {
+		if info.IsDir() {
+			atomic.AddInt32(&folderCount, 1)
+		} else {
+			atomic.AddInt32(&fileCount, 1)
+		}
+	}
+	return nil
+}
+
+// This callback simply counts files and folders and also opens the file in order to artificially cause errors related
+// to exceeding the number of open file handles
+//
+// Note that the callback function should be thread-safe
+// (this is why we use "atomic.AddInt32()" function to increment counters).
+func errorCallback(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		atomic.AddInt32(&errorCount, 1)
+		return err
+	} else {
+		if info.IsDir() {
+			atomic.AddInt32(&folderCount, 1)
+		} else {
+			// open the file in order to artificially cause errors
+			f, _ := os.Open(path)
+			fileSlice = append(fileSlice, f)
+			atomic.AddInt32(&fileCount, 1)
+		}
+	}
+	return nil
+}
+
+func main() {
+	if len(os.Args) < 2 || os.Args[1] == "" {
+		fmt.Println("Usage: traversaltime <directory-to-scan>")
+		os.Exit(0)
+	}
+	dir := os.Args[1]
+
+	// run the concurrent version
+
+	folderCount = 0
+	fileCount = 0
+	errorCount = 0
+
+	fmt.Print("Running concurrent version... ")
+	start := time.Now()
+
+	err := cwalk.Walk(dir, callback)
+
+	fmt.Printf("done in %s\n", time.Since(start))
+	fmt.Printf("\t%d directories found\n", folderCount)
+	fmt.Printf("\t%d files found\n", fileCount)
+	fmt.Printf("\t%d errors detected by the callback\n", errorCount)
+
+	if err != nil {
+		fmt.Printf("\t%d errors returned by cwalk\n", len(err.(cwalk.WalkerErrorList).ErrorList))
+		fmt.Printf("Error :\n%s\n", err.Error())
+	}
+
+	// run the standard (single-threaded) version
+
+	folderCount = 0
+	fileCount = 0
+	errorCount = 0
+
+	fmt.Print("Running standard version... ")
+	start = time.Now()
+
+	err = filepath.Walk(dir, callback)
+
+	fmt.Printf("done in %s\n", time.Since(start))
+	fmt.Printf("\t%d directories found\n", folderCount)
+	fmt.Printf("\t%d files found\n", fileCount)
+	fmt.Printf("\t%d errors detected by the callback\n", errorCount)
+
+	if err != nil {
+		fmt.Printf("Error : %s\n", err.Error())
+	}
+
+	// run the concurrent triggering errors version
+
+	folderCount = 0
+	fileCount = 0
+	errorCount = 0
+
+	fmt.Print("Running concurrent version to trigger errors...\n")
+	fmt.Print("Errors are triggered by exceeding file limits by opening every file... ")
+
+	start = time.Now()
+
+	//start the open files func
+	time.Sleep(1000)
+
+	err = cwalk.Walk(dir, errorCallback)
+
+	close(openFiles)
+
+	fmt.Printf("done in %s\n", time.Since(start))
+	fmt.Printf("\t%d directories found\n", folderCount)
+	fmt.Printf("\t%d files found\n", fileCount)
+	fmt.Printf("\t%d errors detected by the callback\n", errorCount)
+
+	if err != nil {
+		fmt.Printf("\t%d errors returned by cwalk\n", len(err.(cwalk.WalkerErrorList).ErrorList))
+		fmt.Printf("Error :\n%s\n", err.Error())
+	}
+}
